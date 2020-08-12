1dbedcbffa573712aecae67bd0ffa49ca3669bf6
fupengga <fupengga@sina.com>
2020-08-12 15:54:08 +0800

    接雨水

--- src/main/java/algorithm/Rain.java
+++ src/main/java/algorithm/Rain.java
@@ -1,63 +1,40 @@
 package main.java.algorithm;
 
-/**
- * 接雨水问题
- */
 public class Rain {
-
+    /**
+     * 接雨水
+     */
     public int trap(int[] height) {
         int sum = 0;
-        //最两端的列不用考虑，因为一定不会有水。所以下标从 1 到 length - 2
-        for (int i = 1; i < height.length - 1; i++) {
-            int max_left = 0;
-            //找出左边最高
-            for (int j = i - 1; j >= 0; j--) {
-                if (height[j] > max_left) {
-                    max_left = height[j];
+        int max = getMax(height);//找到最大的高度，以便遍历。
+        for (int i = 1; i <= max; i++) {
+            boolean isStart = false; //标记是否开始更新 temp
+            int temp_sum = 0;
+            for (int j = 0; j < height.length; j++) {
+                if (isStart && height[j] < i) {
+                    temp_sum++;
                 }
-            }
-            int max_right = 0;
-            //找出右边最高
-            for (int j = i + 1; j < height.length; j++) {
-                if (height[j] > max_right) {
-                    max_right = height[j];
+                if (height[j] >= i) {
+                    sum = sum + temp_sum;
+                    temp_sum = 0;
+                    isStart = true;
                 }
-            }
-            //找出两端较小的
-            int min = Math.min(max_left, max_right);
-            //只有较小的一段大于当前列的高度才会有水，其他情况不会有水
-            if (min > height[i]) {
-                sum = sum + (min - height[i]);
             }
         }
         return sum;
     }
-
-    /**
-     * 动态规划
-     * @param height
-     * @return
-     */
-    public int trap1(int[] height) {
-        int sum = 0;
-        int[] max_left = new int[height.length];
-        int[] max_right = new int[height.length];
-
-        for (int i = 1; i < height.length - 1; i++) {
-            max_left[i] = Math.max(max_left[i - 1], height[i - 1]);
-        }
-        for (int i = height.length - 2; i >= 0; i--) {
-            max_right[i] = Math.max(max_right[i + 1], height[i + 1]);
-        }
-        for (int i = 1; i < height.length - 1; i++) {
-            int min = Math.min(max_left[i], max_right[i]);
-            if (min > height[i]) {
-                sum = sum + (min - height[i]);
+    private int getMax(int[] height) {
+        int max = 0;
+        for (int i = 0; i < height.length; i++) {
+            if (height[i] > max) {
+                max = height[i];
             }
         }
-        return sum;
+        return max;
     }
 
 
-
+    public static void main(String[] args) {
+        System.out.println(new Rain().trap(new int[]{ 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }));
+    }
 }
